1b410f7ebfa619e4bae1f42476758f2b1af2fe56
Asiful Choudhury <asiful307@gmail.com>
2019-11-09 00:37:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Add your code here
+class Dog
+  @@all  []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all []
+  end 
+  
+end 
