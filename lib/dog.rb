1abe3dc7c9b96bb8780612498f08d1b6b8133454
Henry Allan <henryallan50@gmail.com>
2019-11-12 22:34:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,42 @@
-# Add your code here
+class Dog 
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    
+    @name = name
+    save
+    
+  end
+  
+  def save
+    
+    @@all << self
+    
+  end
+  
+  def self.all
+    
+   @@all
+    
+  end
+  
+  def self.print_all
+    
+    for item in @@all do 
+      
+      puts item.name
+      
+    end
+    
+  end
+  
+  def self.clear_all
+    
+    @@all = []
+    
+  end
+  
+end
